30832983355cf9ac63e283da945b1430d38fd387
Chunjie Zhu <chunjie.zhu@cloud.com>
2025-06-27 14:19:55 +0800

    CP-52200: add driver multi version support
      update copyright
    
    Signed-off-by: Chunjie Zhu <chunjie.zhu@cloud.com>

--- plugins-base/XSFeatureDMV.py
+++ plugins-base/XSFeatureDMV.py
@@ -1,4 +1,4 @@
-# Copyright (c) 2008-2009 Citrix Systems Inc.
+# Copyright (c) 2025 Cloud Software Group, Inc.
 #
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
